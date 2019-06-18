a2d4847d709aa672ebfbdcfec7d68a1bbdf7c3be
Dmitrii Pavlov <delnote@gmail.com>
2019-06-19 00:41:54 +0300

    Exercise 1 added comments, Exercise 2 in process of refactoring.

--- src/test/java/hw1/CalculatorIsNegativeTesting.java
+++ src/test/java/hw1/CalculatorIsNegativeTesting.java
@@ -2,16 +2,14 @@ package hw1;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class CalculatorIsNegativeTesting extends BaseTest {
 
     @Test(groups = { "All", "Second group" }, dataProviderClass = DataProviders.class, dataProvider = "isNegativeData")
     public void isNegativeTesting(long num) {
         System.out.println("isNegative test");
-        boolean actual = calc.isNegative(num);
-        boolean expected = true;
-        assertEquals(actual, expected);
+        assertTrue(calc.isNegative(num));
     }
 
 }
