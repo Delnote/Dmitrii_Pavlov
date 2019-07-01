3fa0df20d7bda36da45ac8f5f642e2b9e0b61322
Dmitrii Pavlov <delnote@gmail.com>
2019-07-01 17:21:32 +0300

    Exercise 4 done and ready to check.

--- src/test/java/hw4/dataProviders/DataProviders.java
+++ src/test/java/hw4/dataProviders/DataProviders.java
@@ -19,7 +19,7 @@ public class DataProviders {
         TestDataBuilder testData1 = TestDataBuilder.builder().Elements(Arrays.asList(EARTH.getName())).Colors(YELLOW.getName()).Metals(GOLD.getName()).build();
         TestDataBuilder testData2 = TestDataBuilder.builder().Odd("3").Even("8").Vegetables(Arrays.asList(CUCUMBER.getName(), TOMATO.getName())).build();
         TestDataBuilder testData3 = TestDataBuilder.builder().Odd("3").Even("2").Elements(Arrays.asList(WATER.getName(), WIND.getName(), FIRE.getName())).Metals(BRONZE.getName()).Vegetables(Arrays.asList(ONION.getName())).build();
-        TestDataBuilder testData4 = TestDataBuilder.builder().Odd("5").Even("6").Elements(Arrays.asList(WATER.getName())).Colors(GREEN.getName()).Metals(SELEN.getName()).Vegetables(Arrays.asList(CUCUMBER.getName(), TOMATO.getName(), VEGETABLES.getName(), ONION.getName())).build();
+        TestDataBuilder testData4 = TestDataBuilder.builder().Odd("5").Even("6").Elements(Arrays.asList(WATER.getName())).Colors(GREEN.getName()).Metals(SELEN.getName()).Vegetables(Arrays.asList(ONION.getName(), TOMATO.getName(), VEGETABLES.getName(), CUCUMBER.getName())).build();
         TestDataBuilder testData5 = TestDataBuilder.builder().Elements(Arrays.asList(FIRE.getName())).Colors(BLUE.getName()).Vegetables(Arrays.asList(CUCUMBER.getName(), TOMATO.getName(), VEGETABLES.getName())).build();
 
 
